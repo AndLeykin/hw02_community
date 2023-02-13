b7b4fc745a353e9c3d223c78207e62f898e3f523
Андрей Лейкин <andleykin@MacBook-Pro-Andrej.local>
2023-02-14 01:43:42 +0300

    второй  коммит

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,21 +1,23 @@
 from django.contrib import admin
-# Из модуля models импортируем модель Post
+
 from .models import Post, Group
 
 
 class PostAdmin(admin.ModelAdmin):
-    # Перечисляем поля, которые должны отображаться в админке
-    list_display = ('pk', 'text', 'pub_date', 'author', 'group')
-    # Меняем поле group в любом посте прямо из списка постов
+    list_display = ('pk', 'text', 'pub_date', 'author', 'group',)
     list_editable = ('group',)
-    # Добавляем интерфейс для поиска по тексту постов
     search_fields = ('text',)
-    # Добавляем возможность фильтрации по дате
     list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
 
 
-# При регистрации модели Post источником конфигурации для неё назначаем
-# класс PostAdmin
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'slug', 'description',)
+    list_editable = ('slug',)
+    search_fields = ('title',)
+    ordering = ('title',)
+    empty_value_display = '-пусто-'
+
+
 admin.site.register(Post, PostAdmin)
-admin.site.register(Group)
+admin.site.register(Group, GroupAdmin)
--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,30 +1,51 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+
 User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField(default='введите описание')
+    title = models.CharField(max_length=200, verbose_name='название')
+    slug = models.SlugField(unique=True, verbose_name='уникальный адрес')
+    description = models.TextField(default='',
+                                   verbose_name='описание сообщества')
 
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name = 'сообщество'
+        verbose_name_plural = 'cообщества'
+
 
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(verbose_name='текст')
+    pub_date = models.DateTimeField(auto_now_add=True,
+                                    verbose_name='дата публикации')
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts'
+        related_name='posts',
+        verbose_name='пользователь'
     )
     group = models.ForeignKey(
         Group,
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
-        related_name='posts'
+        related_name='posts',
+        verbose_name='сообщество'
     )
+
+    def __str__(self):
+        post_description = (f'публикация пользователя {self.author} от '
+                            f'{self.pub_date.strftime("%d.%m.%Y, %H:%M:%S")}: '
+                            f'{self.text[:50]}...'
+                            )
+        return post_description
+
+    class Meta:
+        verbose_name = 'публикация'
+        verbose_name_plural = 'публикации'
+        ordering = ['-pub_date']
